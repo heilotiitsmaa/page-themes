11d1bf823a9a6ed9c4909ad0014ab2bb53196abd
heilotiitsmaa <heilo.tiitsmaa@voco.ee>
2025-04-01 00:23:06 +0300

    komponentide loomine

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>My Application</h1>
+      <button>Toggle Theme</button>
+    </header>
+  )
+}
+export default Header;
---
+++ src/components/Page.jsx
@@ -0,0 +1,15 @@
+import Header from './Header';
+
+const Page = () => {
+  return (
+    <div id='app'>
+      <Header />
+     
+     <article>
+       <h2>Welcome to the App</h2>
+       <p>This is a simple React application.</p>
+       </article>
+    </div>
+  );
+}
+export default Page;
